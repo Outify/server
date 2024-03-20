875007194a30322b037a9860e74150ec1eda7077
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-03-20 17:25:40 +0900

    Feat: Add Hello Controller for check connection

---
+++ src/main/java/com/mcc/outify/apis/HelloController.java
@@ -0,0 +1,13 @@
+package com.mcc.outify.apis;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    String Hello() {
+        return "Hello, world! This is Outify from MCC";
+    }
+}
