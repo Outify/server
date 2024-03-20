f7d71467270342ffdf24e41c1a87fab47f904420
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-03-20 17:21:36 +0900

    Initial commit

---
+++ src/main/java/com/mcc/outify/OutifyApplication.java
@@ -0,0 +1,13 @@
+package com.mcc.outify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcc/outify/OutifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcc.outify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
