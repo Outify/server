3385b869c7bb57c24b37c278adf00119d0d769e6
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-04-26 17:05:50 +0900

    chore: Add WeatherSource & delay option before method execution for application stability & save

--- src/main/java/com/mcc/outify/weathers/WeatherDataRunner.java
+++ src/main/java/com/mcc/outify/weathers/WeatherDataRunner.java
@@ -1,12 +1,19 @@
 package com.mcc.outify.weathers;
 
+import com.mcc.outify.weathers.entity.WeatherSourceEntity;
 import com.mcc.outify.weathers.openApi.OpenMeteoAPI;
 import com.mcc.outify.weathers.openApi.YrAPI;
+import com.mcc.outify.weathers.repository.WeatherDataRepository;
+import com.mcc.outify.weathers.repository.WeatherSourceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -15,28 +22,50 @@ public class WeatherDataRunner implements ApplicationRunner {
     private final OpenMeteoAPI openMeteo;
     private final YrAPI yr;
     private final LocationList locationList;
+    private final WeatherSourceRepository weatherSourceRepository;
+    private final WeatherDataRepository weatherDataRepository;
 
     @Override
     public void run(ApplicationArguments args) {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.schedule(this::executeAfterDelay, 10, TimeUnit.SECONDS);
+    }
+
+    private void executeAfterDelay() {
         locationList.readExcel();
-        excuteWeatherOpenAPIs();
+        saveSource();
+        executeWeatherOpenAPIs();
     }
 
     @Scheduled(cron = "0 5 0 * * ?")
     public void weatherAPIScheduled() {
-        excuteWeatherOpenAPIs();
+        executeWeatherOpenAPIs();
     }
 
-    private void excuteWeatherOpenAPIs() {
+    private void saveSource() {
+        WeatherSourceEntity meteoSource = weatherSourceRepository.findBySource(WeatherSourceEntity.WeatherSource.METEO);
+        if (meteoSource == null) {
+            meteoSource = new WeatherSourceEntity(WeatherSourceEntity.WeatherSource.METEO);
+            weatherSourceRepository.save(meteoSource);
+        }
+
+        WeatherSourceEntity yrSource = weatherSourceRepository.findBySource(WeatherSourceEntity.WeatherSource.YR);
+        if (yrSource == null) {
+            yrSource = new WeatherSourceEntity(WeatherSourceEntity.WeatherSource.YR);
+            weatherSourceRepository.save(yrSource);
+        }
+    }
+
+    private void executeWeatherOpenAPIs() {
         getYrData();
-//        getOpenMeteoData();
+        getOpenMeteoData();
     }
 
     private void getOpenMeteoData() {
         try {
             openMeteo.getWeatherData();
         } catch (Exception e) {
-            System.err.println("An error occurred during weather data execution: " + e.getMessage());
+            throw new RuntimeException("An error occurred during weather data execution: " + e.getMessage(), e);
         }
     }
 
@@ -44,7 +73,7 @@ public class WeatherDataRunner implements ApplicationRunner {
         try {
             yr.getWeatherData();
         } catch (Exception e) {
-            System.err.println("An error occurred during weather data execution: " + e.getMessage());
+            throw new RuntimeException("An error occurred during weather data execution: " + e.getMessage(), e);
         }
     }
 
