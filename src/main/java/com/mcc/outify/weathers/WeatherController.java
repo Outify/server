d9479977a857b273368d50afadc6e82c81ecc29b
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-04-30 19:57:15 +0900

    chore: Change parameter assignment method

--- src/main/java/com/mcc/outify/weathers/WeatherController.java
+++ src/main/java/com/mcc/outify/weathers/WeatherController.java
@@ -2,7 +2,7 @@ package com.mcc.outify.weathers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -14,8 +14,7 @@ public class WeatherController {
     private final WeatherService weatherService;
 
     @GetMapping("/weathers")
-    public Map<String, Object> getLocationWeather(@RequestBody WeatherRequestDto weatherRequestDto) {
-        String addr = weatherRequestDto.getAddr();
+    public Map<String, Object> getLocationWeather(@RequestParam String addr) {
         Map<String, Object> resData = weatherService.getLocationWeather(addr);
 
         return resData;
