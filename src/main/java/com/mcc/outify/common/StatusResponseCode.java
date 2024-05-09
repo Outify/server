5f9259944fa620b4eb15ba9ec18d63411cdb411c
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-05-09 19:54:42 +0900

    feat: Declaration of Common Types for Status Codes

---
+++ src/main/java/com/mcc/outify/common/StatusResponseCode.java
@@ -0,0 +1,17 @@
+package com.mcc.outify.common;
+
+public interface StatusResponseCode {
+
+    String SUCCESS = "SU";
+
+    String VALIDATION_FAIL = "VF";
+
+    String DUPLICATE_ACCOUNT = "DA";
+
+    String SIGN_IN_FAIL = "SF";
+
+    String CERTIFICATION_FAIL = "CF";
+
+    String DATABASE_ERROR = "DBE";
+
+}
