b350faaf1b7930bd0dc8fabb57fad425612578b8
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-08-01 02:17:42 +0900

    fix: ./output.log: Permission denied error

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,5 +3,5 @@
 echo "============ Start Server ============"
 cd /home/ubuntu/server
 sudo fuser -k -n tcp 8080 || true
-nohup java -jar outify.jar > /home/ubuntu/server/output.log 2>&1 &
+nohup java -jar outify.jar > ./output.log 2>&1 &
 echo "=========== End Deployment ==========="
