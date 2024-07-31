21c8ed8b4ebf9f10b5c9050a2138826a7f1500cd
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-08-01 00:31:06 +0900

    refactor: Automated Scalability Deployment

---
+++ scripts/start-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "============ Start Server ============"
+cd /home/ubuntu/server
+sudo fuser -k -n tcp 8080 || true
+nohup java -jar outify.jar > ./output.log 2>&1 &
+echo "=========== End Deployment ==========="
