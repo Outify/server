475ec0f34280035c95ac4166413343698ea56cc3
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-08-05 12:58:29 +0900

    refactor: Automated deployment with Docker, CodeDeploy

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "============ Start Server ============"
-cd /home/ubuntu/server
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar outify.jar > ./output.log 2>&1 &
+docker stop outify-server || true
+docker rm outify-server || true
+docker pull 058264467328.dkr.ecr.ap-northeast-2.amazonaws.com/outify-server:latest
+docker run -d --name outify-server -p 8080:8080 058264467328.dkr.ecr.ap-northeast-2.amazonaws.com/outify-server:latest
 echo "=========== End Deployment ==========="
